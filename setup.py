1aad60cf8578b3a4d6a657a1622c2c1f2f2e4bc2
Unknown <beinbed@hotmail.com>
2017-07-19 15:46:43 +0100

    Updated to 0.6

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'keepasshttplib',
   packages = ['keepasshttplib'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.6',
   description = 'Client for keepasshttp',
   author = 'Tommy Pride',
   author_email = 'tommy@esteponian.co.uk',
   url = 'https://github.com/02TPr1804/keepasshttplib', # use the URL to the github repo
-  download_url = 'https://github.com/02TPr1804/keepasshttplib/archive/0.3.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/02TPr1804/keepasshttplib/archive/0.6.tar.gz', # I'll explain this in a second
   keywords = ['keepass'], # arbitrary keywords
   classifiers = [],
   install_requires=[
